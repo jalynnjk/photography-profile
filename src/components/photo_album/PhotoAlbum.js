78e32f441f3ce1091e3c375fa287f8479c9a75a4
Jalynn <jalynnking@gmail.com>
2022-08-05 11:01:51 -0400

    Create photo album component

---
+++ src/components/photo_album/PhotoAlbum.js
@@ -0,0 +1,15 @@
+import './photo_album.css'
+import { useState } from 'react'
+import axios from 'axios';
+
+function PhotoAlbum(props) {
+    const [album, setAlbum] = useState([])
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default PhotoAlbum;
