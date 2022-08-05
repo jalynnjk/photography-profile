35e992f5caf803c12fe49f2a378c71778ba52409
Jalynn <jalynnking@gmail.com>
2022-08-04 23:42:37 -0400

    Render profile component, make axios call to retrieve data, map data out, and add styling

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,47 @@
+import './profile.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import profilePic from '../assets/img/profile.jpeg'
+
+function Profile(props) {
+	const [photographers, setPhotographers] = useState([]);
+
+	async function getPhotographers() {
+		try {
+			const response = await axios.get(
+				'https://photography-profile-api.herokuapp.com/api/photographers'
+			);
+            setPhotographers(response.data)
+		} catch (error) {
+            console.log(error)
+        }
+	}
+
+    useEffect(() => {
+        getPhotographers()
+    }, [])
+
+	return <div className='profile-component'>
+        {photographers ? (
+        photographers.map((photographer, index) => { return(
+            <div className='profile-container' key={index}>
+            <img src={profilePic} alt='profile picture' className='profile-picture'/>
+            <div className='photographer-info'>
+                <h1 className='photographer-name'>{photographer.name}</h1>
+                <h4 className='bio-title'>Bio</h4>
+                <p className='bio'>{photographer.bio}</p>
+            </div>
+            <div className='contact-info'>
+                <h4 className='phone-title'>Phone</h4>
+                <p className='phone'>{photographer.phone}</p>
+                <h4 className='email-title'>Email</h4>
+                <p className='email'>{photographer.email}</p>
+            </div>
+
+        </div>)
+        })
+        ):<p>photographer is loading</p>}
+    </div>;
+}
+
+export default Profile;
