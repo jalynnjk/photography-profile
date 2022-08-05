35fabbef0e4ce5571b10e1606fc2ee716b029802
Jalynn <jalynnking@gmail.com>
2022-08-05 11:03:22 -0400

    Import photo album component and add initial app styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Profile from './components/profile/Profile';
+import PhotoAlbum from './components/photo_album/PhotoAlbum'
 
 function App() {
 
   return (
     <div className="App">
       <Profile />
+      <PhotoAlbum />
     </div>
   );
 }
