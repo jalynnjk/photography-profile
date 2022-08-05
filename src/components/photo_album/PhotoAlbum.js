53f51e0cb68b31c342bd486ad6cabd0ef47e5fe3
Jalynn <jalynnking@gmail.com>
2022-08-05 15:49:22 -0400

    Add background image and featured icon, hit styling mvp

--- src/components/photo_album/PhotoAlbum.js
+++ src/components/photo_album/PhotoAlbum.js
@@ -1,30 +1,45 @@
 import './photo_album.css'
-import { useState, useEffect, useContext } from 'react'
+import { useContext } from 'react'
 import { DataContext } from '../../dataContext'
+import featured from '../../assets/favoriteselected.png'
 
 
 function PhotoAlbum(props) {
-    const {photographerAlbums, setPhotographerAlbums} = useContext
+    const {photographerAlbums} = useContext
     (DataContext)
 
     return (
-        <div>
+        <div className='photo-album-component'>
             {photographerAlbums ? (
                 photographerAlbums.map((album, index) => {
                 return (
-                    <div key={index}>
-                        {album.photos.map((photo, index) => {
-                            return (
-                                <div key={index}>
-                                    <img src={photo.img} alt="Landscape" className='landscape-image'/>
-                                    <h2 className='photo-title'>{photo.title}</h2>
-                                    <p className='photo-description'>{photo.description}</p>
-                                    <p className='photo-date'>{photo.date}</p>
-                                </div>
-                            )
-                        })}
-                    </div>
-                    )
+									<div key={index} className='album'>
+										{album.photos.map((photo, index) => {
+											return (
+												<div key={index} className='photo-container'>
+													<article
+														className='photo'
+														style={{
+															backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url(${photo.img})`,
+														}}>
+														<h2 className='photo-title'>{photo.title}</h2>
+													</article>
+													<div className='photo-details-background'>
+														<div className='photo-details'>
+															<p className='photo-description'>
+																{photo.description}
+															</p>
+															{photo.featured ? (
+																<img src={featured} className='featured'></img>
+															) : null}
+															<p className='photo-date'>{photo.date}</p>
+														</div>
+													</div>
+												</div>
+											);
+										})}
+									</div>
+								);
                 })
             ) : <p>Album loading</p>}
         </div>
