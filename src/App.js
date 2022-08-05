d55ab99b79481054dfedf6f2bde3c90551c81fb7
Jalynn <jalynnking@gmail.com>
2022-08-05 14:01:46 -0400

    Use react context to pass photographer data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import { useState } from 'react';
+import { DataContext } from './dataContext.js'
 import Profile from './components/profile/Profile';
 import PhotoAlbum from './components/photo_album/PhotoAlbum'
 
 function App() {
+  const [photographerAlbums, setPhotographerAlbums] = useState()
 
   return (
-    <div className="App">
+    <DataContext.Provider className="App"
+    value={{photographerAlbums, setPhotographerAlbums}}
+    >
       <Profile />
       <PhotoAlbum />
-    </div>
+    </DataContext.Provider>
   );
 }
 
