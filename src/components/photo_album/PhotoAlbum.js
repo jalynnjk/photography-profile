36a8f9ce502da50054dc7ba19ac29cb329279729
Jalynn <jalynnking@gmail.com>
2022-08-05 14:02:41 -0400

    Render photo details

--- src/components/photo_album/PhotoAlbum.js
+++ src/components/photo_album/PhotoAlbum.js
@@ -1,13 +1,32 @@
 import './photo_album.css'
-import { useState } from 'react'
-import axios from 'axios';
+import { useState, useEffect, useContext } from 'react'
+import { DataContext } from '../../dataContext'
+
 
 function PhotoAlbum(props) {
-    const [album, setAlbum] = useState([])
+    const {photographerAlbums, setPhotographerAlbums} = useContext
+    (DataContext)
 
     return (
         <div>
-            
+            {photographerAlbums ? (
+                photographerAlbums.map((album, index) => {
+                return (
+                    <div key={index}>
+                        {album.photos.map((photo, index) => {
+                            return (
+                                <div key={index}>
+                                    <img src={photo.img} alt="Landscape" className='landscape-image'/>
+                                    <h2 className='photo-title'>{photo.title}</h2>
+                                    <p className='photo-description'>{photo.description}</p>
+                                    <p className='photo-date'>{photo.date}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    )
+                })
+            ) : <p>Album loading</p>}
         </div>
     );
 }
